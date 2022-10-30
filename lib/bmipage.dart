3c6afcf8c34abe0bd1b889236a82c7ae42335e96
Irene Wang <irenewang@cherricorp.com>
2022-10-30 11:00:39 +0800

    wip

--- lib/bmipage.dart
+++ lib/bmipage.dart
@@ -1,8 +1,14 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:bmi_calculator_app_flutter/resultpage.dart';
 import 'package:bmi_calculator_app_flutter/theame.dart';
-import 'dart:async';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:crypto/crypto.dart';
+import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
 
 class BmiPage extends StatefulWidget {
   @override
@@ -18,6 +24,9 @@ class CommonWebView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Flutter WebView example'),
@@ -76,12 +85,21 @@ class _BmiPageState extends State<BmiPage> {
 
   @override
   Widget build(BuildContext context) {
+    var client  =  GrailApiClient(
+        httpClient: http.Client(),
+        baseUrl: "http://alpha.api.g2rail.com",
+        apiKey: "fa656e6b99d64f309d72d6a8e7284953",
+        secret: "9a52b1f7-7c96-4305-8569-1016a55048bc");
+    var asyncKey = client.getSolutions("BERLIN", "FRANKFURT", "2022-10-31", "10:00", 1, 0);
+    print(asyncKey);
+
     return Container(
       color: secondary,
       child: Column(
         children: <Widget>[
           AppBar(
-            title: Text("新天鵝堡"),
+            // title: Text("新天鵝堡"),
+            title: Text(asyncKey.toString()),
             elevation: 0,
             backgroundColor: Colors.transparent,
           ),
@@ -147,5 +165,103 @@ class _BmiPageState extends State<BmiPage> {
         ],
       ),
     );
+  }
+}
+
+class SearchCriteria {
+  String from;
+  String to;
+  String date;
+  String time;
+  int adult;
+  int child;
+
+  SearchCriteria(from, to, date, time, adult, child) {
+    this.from = from;
+    this.to = to;
+    this.date = date;
+    this.time = time;
+    this.adult = adult;
+    this.child = child;
+  }
+
+  String toQuery() {
+    return "from=$from&to=$to&date=$date&time=$time&adult=$adult&child=$child";
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "from": from,
+      "to": to,
+      "date": date,
+      "time": time,
+      "adult": adult,
+      "child": child
+    };
+  }
+}
+
+class GrailApiClient {
+  final baseUrl;
+  final apiKey;
+  final secret;
+  final http.Client httpClient;
+
+  GrailApiClient({@required this.httpClient,
+    @required this.baseUrl,
+    @required this.apiKey,
+    @required this.secret})
+      : assert(httpClient != null);
+
+
+  Map<String, String> getAuthorizationHeaders(Map<String, dynamic> params) {
+    var timestamp = DateTime.now();
+    params['t'] = (timestamp.millisecondsSinceEpoch ~/ 1000).toString();
+    params['api_key'] = apiKey;
+
+    var sortedKeys = params.keys.toList()
+      ..sort((a, b) => a.compareTo(b));
+    StringBuffer buffer = StringBuffer("");
+    sortedKeys.forEach((key) {
+      if (params[key] is List || params[key] is Map) return;
+      buffer.write('$key=${params[key].toString()}');
+    });
+    buffer.write(secret);
+
+    String hashString = buffer.toString();
+    String authorization = md5.convert(utf8.encode(hashString)).toString();
+
+    return {
+      "From": apiKey,
+      "Content-Type": 'application/json',
+      "Authorization": authorization,
+      "Date": HttpDate.format(timestamp),
+      "Api-Locale": "zh-TW"
+    };
+  }
+
+  Future<dynamic> getSolutions(from, to, date, time, adult, child) async {
+    final criteria = SearchCriteria(from, to, date, time, adult, child);
+    final solutionUrl =
+        '$baseUrl/api/v2/online_solutions/?${criteria.toQuery()}';
+
+    final solutionResponse = await this
+        .httpClient
+        .get(Uri.parse(solutionUrl), headers: getAuthorizationHeaders(criteria.toMap()));
+
+    if (solutionResponse.statusCode != 200) {
+      throw Exception('error getting solutions');
+    }
+
+    final solutionsJson = jsonDecode(solutionResponse.body);
+
+    return solutionsJson;
+  }
+
+  Future<dynamic> getAsyncResult(String asyncKey) async {
+    final asyncResultURl = '$baseUrl/api/v2/async_results/$asyncKey';
+    final asyncResult = await this.httpClient.get(Uri.parse(asyncResultURl),
+        headers: getAuthorizationHeaders({"async_key": asyncKey}));
+    return {"data": jsonDecode(utf8.decode(asyncResult.bodyBytes))};
   }
 }
