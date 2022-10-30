08b5947c6aedcf8d7c6aa6b860ab803589f7df01
EricChenHuanWen <munga0923@gmail.com>
2022-10-30 14:55:02 +0800

    Implement fake data

--- lib/services/firebase.dart
+++ lib/services/firebase.dart
@@ -7,7 +7,23 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class FireBase
 {
   var country = ['French','Spain','Belgium'];
+  var travelView = ['NewSwanCastle','Hell','Heaven'];
   List<String> get GetCountry{
     return country;
+  }
+  List<String> GetLocation(String Country, String language){
+    switch (Country.toLowerCase()) {
+      case 'french':
+        return ['LoveCity','CityLove','MakeCity'];
+        break;
+      case 'spain':
+        return ['Hell','Heaven','RealCity'];
+        break;
+      case 'belgium':
+        return ['CarCity','PlaneCity','BikeCity'];
+        break;
+      default:
+        return [];
+    }
   }
 }
