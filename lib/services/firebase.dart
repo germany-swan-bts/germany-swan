f882384b37374b84fbdda63e5fc7236c9881189d
EricChenHuanWen <munga0923@gmail.com>
2022-10-30 12:41:54 +0800

    Add firebase stuffs

---
+++ lib/services/firebase.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+//Import firestore database
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FireBase
+{
+  var country = ['French','Spain','Belgium'];
+}
